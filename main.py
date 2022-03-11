e1892f5a1b7668a15d88e6435ca67858d4754eb3
Emma Bates <100385290+ebates1977@users.noreply.github.com>
2022-03-11 16:31:13 +0000

    finished

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 import math
 
-def solve_quadratic(a, b, c):
-  x1 = (-b + math.sqrt() / (2*a)) 
-  x2 = (-b - math.sqrt() / (2*a)) 
-  
-  print("The roots of y = " + str(a) + "x^2 + " + str(b) + "x + " + str(c) + " are: " + str(x1) + " and " + str(x2))
+def m():
+  print("#   #")
+  print("## ##")
+  print("# # #")
+  print("#   #")
+  print("#   #")
+  print()
 
-solve_quadratic(1, 1, 0)     
-# should print: The roots of y = 1x^2 + 1x + 0 are 0.0 and -1.0
-# solve_quadratic(2, 3, 1)     
-# should print: The roots of y = 2x^2 + 3x + 1 are -0.5 and -1.0
-# solve_quadratic(1, 0, -1)    
-# should print: The roots of y = 1x^2 + 0x + -1 are 1.0 and -1.0
+def i():
+  print("###")
+  print(" # ")
+  print(" # ")
+  print(" # ")
+  print("###")
+  print()
+  
+
+def s():
+  print(" ####")
+  print("#    ")
+  print(" ### ")
+  print("    #")
+  print("####  ")
+  print()
+
+def p():
+  print("###")
+  print("#  #")
+  print("###")
+  print("#")
+  print("#")
+  print()
+  
+
+m()
+i()
+s()
+s()
+i()
+s()
+s()
+i()
+p()
+p()
+i()
